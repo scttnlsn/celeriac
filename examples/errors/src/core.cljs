c8bf9448a36608a9d9271f3c7dccff50cf2d1a81
Scott Nelson <scott@scttnlsn.com>
2015-04-26 09:34:57 -0400

    Added catch+ form to errors macro

--- examples/errors/src/core.cljs
+++ examples/errors/src/core.cljs
@@ -2,28 +2,48 @@
   (:require-macros [celeriac.errors :refer [try+]]
                    [cljs.core.match.macros :refer [match]])
   (:require [cljs.core.match]
-            [celeriac.errors :as errors]))
+            [celeriac.errors :refer [info make-error maybe-throw throw+]]))
 
 (enable-console-print!)
 
-(errors/maybe-throw "foo")
+;; Doesn't throw if the given argument is
+;; not an error
+(maybe-throw "foo")
 
+;; Throw and catch a basic error
 (try
-  (errors/maybe-throw (js/Error.))
+  (maybe-throw (js/Error.))
   (catch js/Error e
     (println "caught:" e)))
 
-(println (errors/info (errors/make-error {:foo "bar"})))
+;; Make an error with metadata
+;; Extract the metadata from the rror
+(println (info (make-error {:foo "bar"})))
 
+;; Make and throw an error with the given metadata
+;; Extract the metadata once caught
 (try
-  (errors/throw+ {:foo "bar"})
+  (throw+ {:foo "bar"})
   (catch js/Error e
-    (println "caught:" (errors/info e))))
+    (println "caught:" (info e))))
 
+;; Throw an error with the given metadata
+;; Match on the metadata once caught
+;; `e` bound to error metadata
 (try+
-  (errors/throw+ {:foo "bar"})
-  (catch e
-    (match [e]
-      [{:foo "baz"}] (errors/throw+ e)
-      [{:foo _}] (println "try+ caught:" e)
-      :else (errors/throw+ e))))
+ (throw+ {:foo "bar"})
+ (catch e
+     (match [e]
+            [{:foo "baz"}] (throw+ e)
+            [{:foo _}] (println "try+/catch caught:" e)
+            :else (throw+ e))))
+
+;; Throw an error with the given metadata
+;; Automatically match the metadata once caught
+;; `e` bound to error metadata
+(try+
+ (throw+ {:foo "bar"})
+ (catch+ e
+         [{:foo "baz"}] (throw+ e)
+         [{:foo _}] (println "try+/catch+ caught:" e)
+         :else (throw+ e)))
--- src/celeriac/errors.clj
+++ src/celeriac/errors.clj
@@ -1,13 +1,25 @@
 (ns celeriac.errors)
 
+(defn- parse-catch [form]
+  (let [binding (second form)
+        body (drop 2 form)]
+    `(catch js/Error e#
+       (let [~binding (celeriac.errors/info e#)]
+         ~@body))))
+
+(defn- parse-catch+ [form]
+  (let [binding (second form)
+        body (drop 2 form)]
+    `(catch js/Error e#
+       (let [~binding (celeriac.errors/info e#)]
+         (cljs.core.match.macros/match [~binding]
+                                       ~@body)))))
+
 (defn- parse-try+ [body]
   (for [form body]
-    (if (= (first form) 'catch)
-      (let [name (second form)
-            body (drop 2 form)]
-        `(catch js/Error e#
-           (let [~name (celeriac.errors/info e#)]
-             ~@body)))
+    (condp = (first form)
+      'catch (parse-catch form)
+      'catch+ (parse-catch+ form)
       form)))
 
 (defmacro try+ [& body]
